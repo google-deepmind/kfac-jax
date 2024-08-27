Fix progress off by one.
Progress should be measured by number of gradient steps taken and be equal to 1.0 at end of training. This change also reconciles train progress with eval progress.

PiperOrigin-RevId: 667922779

examples/training.py
@@ -567,9 +567,9 @@ class SupervisedExperiment(abc.ABC):
     if "aux" in stats:
       stats.update(stats.pop("aux", {}))
 
-    stats["progress"] = self.progress(self._python_step)
-
     self._python_step += 1
+
+    stats["progress"] = self.progress(self._python_step)
 
     for name in self.config.get("per_device_stats_to_log", []):
       gathered_stat = jnp.reshape(